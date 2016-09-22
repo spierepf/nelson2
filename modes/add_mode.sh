eda5ab67c7330fa97a56f0d3addb117bb970f3f1
Peter Spierenburg <peter.spierenburg@nautel.com>
2016-09-22 09:28:55 -0300

    Updated add_mode.sh

--- modes/add_mode.sh
+++ modes/add_mode.sh
@@ -7,4 +7,32 @@ cat <<EOF > $1/config/$1.yaml
 
 mode:
   priority: 100
+# code: $1.$1
+  start_events: $1_selected
+  stop_events: $1_complete, $1_failed
+
+logic_blocks:
+  sequences:
+    $1:
+      events:
+        - s_test_3_active
+      events_when_complete: $1_complete, carousel_mode_complete
+EOF
+
+touch $1/__init__.py
+mkdir $1/code
+touch $1/code/__init__.py
+
+cat <<EOF > $1/code/$1.py
+from mpf.system.mode import Mode
+
+class $1(Mode):
+    def mode_init(self):
+        pass
+
+    def mode_start(self, **kwargs):
+        pass
+
+    def mode_stop(self, **kwargs):
+        pass
 EOF
