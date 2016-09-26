982648c6adea295e09adf39e42c78974352659ed
Peter Spierenburg <peter.spierenburg@nautel.com>
2016-09-26 16:46:13 -0300

    End of ball bonus mode now with actual math

--- modes/bonus/code/bonus.py
+++ modes/bonus/code/bonus.py
@@ -1,47 +1,16 @@
 from mpf.system.mode import Mode
-from mpf.system.timing import Timer
 
 class Bonus(Mode):
     def mode_init(self):
-        self.count_experience_points_timer = Timer(callback=self.add_experience_point, frequency=1.0/10.0)
-        self.apply_multiplier_timer = Timer(callback=self.apply_multiplier, frequency=1.0)
-        self.compute_bonus_timer = Timer(callback=self.compute_bonus, frequency=1.0)
+        pass
 
     def mode_start(self, **kwargs):
-        self.log.info("Starting Bonus Mode")
-        if not self.player.is_player_var('experience_points'):
-            self.player.experience_points = 10000000
-
         self.player.counted_experience_points = 0
-        self.player.counted_multiplier = 0
-        self.player.counted_bonus = 0
+        self.player.previous_score = self.player.score
+        self.player.score += self.player.jackpot_value
 
-        self.count_experience_points()
+        self.machine.events.post("count_experience_points")
+        pass
 
     def mode_stop(self, **kwargs):
         pass
-
-    def count_experience_points(self):
-        self.log.info("Counting Experience Points")
-        self.machine.timing.add(self.count_experience_points_timer)
-
-    def add_experience_point(self):
-        self.player.counted_experience_points += 1000000
-        self.machine.events.post("bonus_update_experience_points")
-        if self.player.counted_experience_points == self.player.experience_points:
-            self.machine.timing.remove(self.count_experience_points_timer)
-            self.machine.timing.add(self.apply_multiplier_timer)
-
-    def apply_multiplier(self):
-        self.log.info("Applying Multiplier")
-        self.player.counted_multiplier = 2
-        self.machine.events.post("bonus_update_multiplier")
-        self.machine.timing.remove(self.apply_multiplier_timer)
-        self.machine.timing.add(self.compute_bonus_timer)
-
-    def compute_bonus(self):
-        self.log.info("Computing Bonus")
-        self.player.counted_bonus = self.player.counted_multiplier * self.player.counted_experience_points
-        self.machine.events.post("bonus_update_bonus")
-        self.machine.timing.remove(self.compute_bonus_timer)
-
--- scriptlets/player_experience_points_handler.py
+++ scriptlets/player_experience_points_handler.py
@@ -1,9 +1,12 @@
 from mpf.system.scriptlet import Scriptlet
+from mpf.system.timing import Timer
+
 import math
 
 class PlayerExperiencePointsHandler(Scriptlet):
     def on_load(self):
-        self.experience_points_multipliers = [1, 2, 3, 5]
+        self.experience_point_value = 1000
+        self.experience_point_multipliers = [1, 2, 3, 5]
 
         self.xp_bar_leds = [
             self.machine.leds['l_xp_bar_a'],
@@ -58,8 +61,11 @@ class PlayerExperiencePointsHandler(Scriptlet):
 
         self.machine.events.add_handler("award_jackpot", self.handle_award_jackpot)
         self.machine.events.add_handler("player_experience_points", self.handle_player_experience_points)
-        self.machine.events.add_handler("player_experience_points_multiplier", self.handle_player_experience_points_multiplier)
+        self.machine.events.add_handler("player_experience_point_multiplier_index", self.handle_player_experience_point_multiplier_index)
         self.machine.events.add_handler("upper_lanes_complete", self.handle_upper_lanes_complete)
+        self.machine.events.add_handler("count_experience_points", self.handle_count_experience_points)
+
+        self.count_experience_points_timer = Timer(callback=self.count_experience_point, frequency=1.0/10.0)
 
     def fill_xp_bar(self, value):
         if value < len(self.xp_bar_leds):
@@ -75,22 +81,51 @@ class PlayerExperiencePointsHandler(Scriptlet):
             self.xp_bar_leds[i].color(fg if i < fg_count else bg)
 
     def handle_award_jackpot(self, **kwargs):
-    	player = self.machine.game.player
-    	player.score += 1000 * player.experience_points * self.experience_points_multipliers[player.experience_points_multiplier]
+        player = self.machine.game.player
+        player.score += player.jackpot_value
+
+    def update_jackpot_value(self):
+        player = self.machine.game.player
+
+        if not player.is_player_var('experience_points'):
+            player.experience_points = 0
+            self.handle_player_experience_points()
+
+        if not player.is_player_var('experience_point_multiplier_index'):
+            player.experience_points_multiplier_index = 0
+            self.handle_player_experience_point_multiplier_index()
+
+        player.jackpot_value = self.experience_point_value * player.experience_points * player.experience_point_multiplier
+        self.log.info("Player Jackpot Value: {}".format(player.jackpot_value))
 
     def handle_player_experience_points(self, **kwargs):
         self.fill_xp_bar(self.machine.game.player.experience_points)
+        self.update_jackpot_value()
 
-    def handle_player_experience_points_multiplier(self, **kwargs):
+    def handle_player_experience_point_multiplier_index(self, **kwargs):
         player = self.machine.game.player
         for i in range(0, len(self.xp_multiplier_leds)):
-            c = [0, 255, 0] if i < player.experience_points_multiplier else [0, 0, 0]
+            c = [0, 255, 0] if i < player.experience_point_multiplier_index else [0, 0, 0]
             for led in self.xp_multiplier_leds[i]:
                 led.color(c)
+        player.experience_point_multiplier = self.experience_point_multipliers[player.experience_point_multiplier_index]
+        self.update_jackpot_value()
 
     def handle_upper_lanes_complete(self, **kwargs):
         player = self.machine.game.player
-        if player.experience_points_multiplier < len(self.experience_points_multipliers)-1:
-            player.experience_points_multiplier += 1
+        if player.experience_point_multiplier_index < len(self.experience_point_multipliers)-1:
+            player.experience_point_multiplier_index += 1
         else:
-        	self.machine.events.post("award_jackpot")
+            self.machine.events.post("award_jackpot")
+
+    def handle_count_experience_points(self, **kwargs):
+        self.log.info("Counting Experience Points")
+        self.machine.timing.add(self.count_experience_points_timer)
+
+    def count_experience_point(self):
+        player = self.machine.game.player
+        player.counted_experience_points += 1
+        player.counted_experience_points_value += self.experience_point_value
+        if player.counted_experience_points == player.experience_points:
+            self.machine.timing.remove(self.count_experience_points_timer)
+            self.machine.events.post("count_experience_points_complete")
