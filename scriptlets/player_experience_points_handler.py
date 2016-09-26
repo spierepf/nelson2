92ea9ae29bdb09b980f4195b239e69502b3b9c3f
Peter Spierenburg <peter.spierenburg@nautel.com>
2016-09-26 13:32:58 -0300

    Added code to illuminate xp_bar

--- scriptlets/player_experience_points_handler.py
+++ scriptlets/player_experience_points_handler.py
@@ -1,4 +1,5 @@
 from mpf.system.scriptlet import Scriptlet
+import math
 
 class PlayerExperiencePointsHandler(Scriptlet):
     def on_load(self):
@@ -13,6 +14,16 @@ class PlayerExperiencePointsHandler(Scriptlet):
             self.machine.leds['l_xp_bar_f'],
             self.machine.leds['l_xp_bar_g'],
             self.machine.leds['l_xp_bar_h']
+        ]
+
+        self.xp_bar_colours = [
+            [255, 0, 0],        # RED
+            [255, 127, 0],      # ORANGE
+            [255, 255, 0],      # YELLOW
+            [0, 255, 0],        # GREEN
+            [0, 0, 255],        # BLUE
+            [75, 0, 130],       # INDIGO
+            [127, 0, 255]       # VIOLET
         ]
 
         self.xp_multiplier_leds = [
@@ -49,12 +60,25 @@ class PlayerExperiencePointsHandler(Scriptlet):
         self.machine.events.add_handler("player_experience_points", self.handle_player_experience_points)
         self.machine.events.add_handler("upper_lanes_complete", self.handle_upper_lanes_complete)
 
+    def fill_xp_bar(self, value):
+        if value < len(self.xp_bar_leds):
+            bg = [0,0,0]
+            fg = self.xp_bar_colours[0]
+            fg_count = value
+        else:
+            bg = self.xp_bar_colours[int(math.floor(value / len(self.xp_bar_leds)) - 1) % len(self.xp_bar_colours)]
+            fg = self.xp_bar_colours[int(math.floor(value / len(self.xp_bar_leds))) % len(self.xp_bar_colours)]
+            fg_count = value % len(self.xp_bar_leds)
+
+        for i in range(0, len(self.xp_bar_leds)):
+            self.xp_bar_leds[i].color(fg if i < fg_count else bg)
+
     def handle_award_jackpot(self, **kwargs):
     	player = self.machine.game.player
     	player.score += 1000 * player.experience_points * self.experience_points_multipliers[player.experience_points_multiplier]
 
     def handle_player_experience_points(self, **kwargs):
-        self.log.info("handle_player_experience_points")
+        self.fill_xp_bar(self.machine.game.player.experience_points)
 
     def handle_upper_lanes_complete(self, **kwargs):
         player = self.machine.game.player
