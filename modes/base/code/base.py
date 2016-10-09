727849b5be5c72a4b4dcb0870e83e7859ca15654
Peter-Frank Spierenburg <spierepf@hotmail.com>
2016-10-09 00:37:50 -0300

    Updated extra_ball_threshold

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -5,7 +5,7 @@ from mpf.devices.ball_save import BallSave
 class base(Mode):
     def mode_init(self):
         self.log.info("Initializing Base Mode")
-        self.extra_ball_threshold = 50000
+        self.extra_ball_threshold = 5000000
         self.ball_save_leds = [
             self.machine.leds['l_ball_save_s'],
             self.machine.leds['l_ball_save_r'],
